b224bb03de01c8f2b1493602d251d139ae6f3708
cypekdev <mojcyprian8@gmail.com>
2025-06-08 14:38:05 +0200

    changed way to display logo; added colors to logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,139 @@
 import { useState } from 'react'
 import '../App.css'
 
-export default function Home() {
-  return (
-    <>
-<pre>{`
-  _______     _______  ______ _  __   _
- / ____\\ \\   / /  __ \\|  ____| |/ /  | |
-| |     \\ \\_/ /| |__) | |__  | ' / __| | _____   __
-| |      \\   / |  ___/|  __| |  < / _\` |/ _ \\ \\ / /
-| |____   | |  | |    | |____| . \\ (_| |  __/\\ V /
- \\_____|  |_|  |_|    |______|_|\\_\\__,_|\\___| \\_/
-`}
-<pre className='blur'>{`
-  _______     _______  ______ _  __   _
- / ____\\ \\   / /  __ \\|  ____| |/ /  | |
-| |     \\ \\_/ /| |__) | |__  | ' / __| | _____   __
-| |      \\   / |  ___/|  __| |  < / _\` |/ _ \\ \\ / /
-| |____   | |  | |    | |____| . \\ (_| |  __/\\ V /
- \\_____|  |_|  |_|    |______|_|\\_\\__,_|\\___| \\_/
-`}</pre></pre>
+function W({children}) {
+  return <span className='whiteText'>{children}</span>;
+}
 
-    </>
-  )
+function B({children}) {
+  return <span className='blueText'>{children}</span>;
+}
+
+function G({children}) {
+  return <span className='greenText'>{children}</span>;
+}
+
+function P({children}) {
+  return <span className='purpleText'>{children}</span>;
+}
+
+function Pk({children}) {
+  return <span className='pinkText'>{children}</span>;
+}
+
+export default function Home() {
+  return (<>
+    <LogoElement />
+    {/* <div className='blur'>{getLogoElement()}</div> */}
+  </>)
+}
+
+
+function LogoElement() {
+
+  
+  const letterC = [
+    '  _____',
+    ' / ____',
+    '| |    ',
+    '| |    ',
+    '| |____',
+    ' \\_____|', 
+  ] 
+
+  const letterY = [
+    '__     __',
+    '\\ \\   / /',
+    ' \\ \\_/ /',
+    '  \\   / ',
+    '   | |  ',
+    '  |_|  ',
+  ]
+
+
+  const letterP = [
+    '_____ ',
+    '  __ \\',
+    '| |__) ',
+    '|  ___/',
+    '| |    ',
+    '|_|    ',
+  ]
+
+  const letterE = [
+    ' ______',
+    '|  ____',
+    '| |__  ',
+    '|  __| ',
+    '| |____',
+    '|______',
+  ]
+
+  const letterK = [
+    ' _  __',
+    '| |/ /',
+    '| \' / ',
+    '|  < ',
+    '| . \\',
+    '|_|\\_\\',
+  ]
+
+  const letter_d = [
+    '   _ ',
+    '  | |',
+    '__| |',
+    '/ _` |',
+    ' (_| |',
+    '__,_|'
+  ]
+
+  const letter_e = [
+    '',
+    '',
+    ' ___',
+    '/ _ \\',
+    '  __/',
+    '\\___|'
+  ]
+
+  const letter_v = [
+    '',
+    '',
+    '__   __',
+    ' \\ / /',
+    '\\ V /',
+    ' \\_/',
+  ]
+
+  
+  const logoRows = [];
+
+  for (let row = 0; row < 6 ; row++) {
+    const element = 6;
+    
+    logoRows.push(<>
+      <W>{ letterC[row] }</W>
+      <B>{ letterY[row] }</B>
+      <G>{ letterP[row] }</G>
+      <W>{ letterE[row] }</W>
+      <B>{ letterK[row] }</B>
+      <P>{ letter_d[row] }</P>
+      <Pk>{ letter_e[row] }</Pk>
+      <P>{ letter_v[row] }</P>
+    </>)
+  }
+
+
+  console.log(logoRows);
+
+
+  const content = logoRows.map((row, index) => {
+        return <span key={index}>{row}<br /></span>;
+      })
+
+
+  return (<div className='logo'>
+    <pre className='blur'>{ content }</pre>
+    <pre>{ content }</pre>
+  </div>)
 }
