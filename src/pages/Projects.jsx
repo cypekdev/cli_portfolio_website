1b1f35f61d02fc6d39458cb90e18dbff0c2b1750
Cyprian Moj (cypekdev) <105518009+cypekdev@users.noreply.github.com>
2025-10-07 23:43:40 +0200

    created project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,46 @@
-export default function Projects() {
+import { useEffect, useState } from "react"
+import "./Projects.css"
+import SpotlightCard from "../components/SpotlightCard"
+
+function ProjectCard({ imgSrc, title, alias, visitUrl, children }) {
+  const [hovered, setHovered] = useState(false)
+  
   return (
-    <h1>Projects</h1>
+    <SpotlightCard className="project-card dotted-bg" spotlightColor="#8c00ff0e">
+      <div 
+        onMouseOver={()=>setHovered(true)}
+        onMouseLeave={()=>setHovered(false)}
+        className="card-content">
+        <img src={imgSrc} alt={title} className="card-image" />
+        <h2>{title}</h2>
+        
+        <div className="card-description">{children}</div>
+
+      </div>
+    </SpotlightCard>
+  )
+}
+
+export default function Projects() {
+  const [projects, setProjects] = useState([])
+  
+  useEffect(() => {
+    fetch("/data/projects.json")
+      .then(res => res.json())
+      .then(setProjects)
+  }, [])
+  
+  return (
+    <div>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <ProjectCard 
+            key={index} 
+            title={project.title} 
+            imgSrc={project.image}
+            >{project.description}</ProjectCard>
+        ))}
+      </div>
+    </div>
   )
 }
