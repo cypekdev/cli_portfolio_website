145998bad313c8967559116bbee89fc1a0a3f010
Cyprian Moj (cypekdev) <105518009+cypekdev@users.noreply.github.com>
2025-10-07 23:45:48 +0200

    added better nav to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
+import Navigation from "./components/Navigation"
 import Home from "./pages/Home"
-import News from "./pages/News"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
@@ -11,11 +10,10 @@ export default function App() {
     // <h1>CYPEKdev</h1>
 
     <BrowserRouter>
-      <Header/>
+      <Navigation/>
       
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/news" element={<News/>} />
         <Route path="/projects" element={<Projects/>} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
