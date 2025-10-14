ec1877ade6825156cc716794bc6789c21acbdeaa
Cyprian Moj (cypekdev) <105518009+cypekdev@users.noreply.github.com>
2025-10-15 00:41:37 +0200

    Added animated background

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
+import DarkVeil from "./DarkVeil"
 
 export default function App() {
   return (
-    // <h1>CYPEKdev</h1>
 
     <BrowserRouter>
-      <Navigation/>
+      <div style={{ width: '100%', minHeight: '100vh', position: 'absolute' }}>
+        <div style={{ position: "absolute",  width: "100%", height: "100%" }}>      
+          <DarkVeil hueShift={14} opacity={0.4}/>
+        </div>
+
+        <Navigation/>
       
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+
+      </div>
     </BrowserRouter>
+
   )
+
 }
